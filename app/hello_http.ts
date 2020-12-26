388fa86b3a027ba0ddbdc6e1440433aeceba84cd
Totoro-moroku <shinsi4545@gmail.com>
2020-12-27 02:43:40 +0900

    fix std module version

--- app/hello_http.ts
+++ app/hello_http.ts
@@ -1,6 +1,9 @@
-import { serve } from "https://deno.land/std@0.50.0/http/server.ts";
+import { serve } from "https://deno.land/std@0.82.0/http/server.ts";
+
 const s = serve({ port: 8080 });
+
 console.log("http://localhost:8080/");
+
 for await (const req of s) {
   req.respond({ body: "<h1>Hello Deno</h1>\n" });
 }
